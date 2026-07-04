21ee3ef1ff0ea12027d51fa5bb1db59f41d1d69c
Rhiana <rhianah@fastmail.com>
2026-07-04 14:44:11 +1000

    adding base folder component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Folder } from './folder/folder';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Folder],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/folder/folder.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-folder',
+  imports: [],
+  templateUrl: './folder.html',
+  styleUrl: './folder.scss',
+})
+export class Folder {}
