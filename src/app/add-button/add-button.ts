934ee3a95aafa092300c7eacd23bee75f2b31d33
Rhiana <rhianah@fastmail.com>
2026-07-05 11:01:37 +1000

    add toggle to add folder button

--- src/app/add-button/add-button.ts
+++ src/app/add-button/add-button.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
 import { AddFolder } from '../add-folder/add-folder';
 
 @Component({
   selector: 'app-add-button',
   imports: [AddFolder],
-  templateUrl: './add-button.html',
+  template: `
+    <section>
+      <form>
+        <button class="add-button" type="button" (click)="toggleForm()">
+          Add folder to root
+        </button>
+      </form>
+      @if (showAddFolderForm()) {
+        <app-add-folder />
+      }
+    </section>
+  `,
   styleUrl: './add-button.scss',
 })
-export class AddButton { }
+export class AddButton {
+  showAddFolderForm = signal(false)
+
+  toggleForm() {
+    if (this.showAddFolderForm() === true) {
+      this.showAddFolderForm.set(false)
+    } else {
+      this.showAddFolderForm.set(true)
+    }
+  }
+}
