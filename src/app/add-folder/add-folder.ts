8c0f39c04e7ddb070b4f072e88148848ab309577
Rhiana <rhianah@fastmail.com>
2026-07-05 12:09:25 +1000

    adding in base save and cancel button functionality

--- src/app/add-folder/add-folder.ts
+++ src/app/add-folder/add-folder.ts
@@ -7,22 +7,43 @@ import { NodeModel } from '../node/node.model';
   imports: [FormField],
   template: `
     <section>
-      <form class="add-folder-form">
-        <img src="assets/images/folder-open-regular.svg" alt="" height="25px" width="25px" />
-        <input
-          type="text"
-          aria-label="Folder name"
-          placeholder="Folder name"
-          [formField]="folderForm.name"
-        />
-        <button class="add-button" type="button" aria-label="Save">
-          <i class="bx bx-check"></i>
-        </button>
-        <button class="cancel-button" type="button" aria-label="Cancel">
-          <i class="bx bx-x"></i>
-        </button>
-      </form>
-      <p>{{ folderForm.name().value() }}</p>
+      @if (showFolderForm()) {
+        <form class="add-folder-form">
+          <img src="assets/images/folder-open-regular.svg" alt="" height="25px" width="25px" />
+          <input
+            type="text"
+            aria-label="Folder name"
+            placeholder="Folder name"
+            [formField]="folderForm.name"
+          />
+          <button
+            class="add-button"
+            type="button"
+            aria-label="Save"
+            (click)="saveForm()"
+          >
+            <i class="bx bx-check"></i>
+          </button>
+          <button
+            class="cancel-button"
+            type="button"
+            aria-label="Cancel"
+            (click)="cancelForm()"
+          >
+            <i class="bx bx-x"></i>
+          </button>
+        </form>
+      }
+      @if (showFormName()) {
+        <ul class="folder-list">
+          <li>
+            <div class="folder-list__item">
+              <img src="assets/images/folder-open-regular.svg" alt="Folder" height="20px" width="20px" />
+              <span>{{ folderForm.name().value() }}</span>
+            </div>
+          </li>
+        </ul>
+      }
     </section>
   `,
   styleUrl: './add-folder.scss',
@@ -36,4 +57,16 @@ export class AddFolder {
   })
 
   folderForm = form(this.folderModel);
+
+  showFormName = signal(false)
+  showFolderForm = signal(true)
+
+  saveForm() {
+    this.showFormName.set(true)
+    this.showFolderForm.set(false)
+  }
+
+  cancelForm() {
+    this.showFolderForm.set(false)
+  }
 }
